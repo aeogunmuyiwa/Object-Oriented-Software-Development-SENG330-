f1bfbd583edee98d461019619924840f31ee97d7
Adebayo Ogunmuyiwa <aeogunmuyiwa@gmail.com>
2018-02-27 10:13:07 -0800

    Object Oriented Development:Seng330

---
+++ car_share/auto/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Customer(models.Model):
+	name = models.CharField(max_length=200)
+	address = models.CharField(max_length=200)
+	phone_number = models.CharField(max_length=200)
+	email = models.CharField(max_length=200)
+	licence = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+
